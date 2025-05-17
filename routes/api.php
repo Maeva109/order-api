d3c494fb2ce2f12d4a2fe33cca8f5c0956e83efd
Maeva108 <makuetchemaeva9@gmail.com>
2025-05-17 11:35:19 +0100

    test with postman

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('v1')->group(function () {
-    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-        return $request->user();
-    });
-
     Route::apiResource('books', \App\Http\Controllers\API\BooksController::class);
 });
